3c2a254e8aa409659007a5f991a21940357dff35
谭杰 <798750509@qq.com>
2016-05-23 16:44:25 +0800

    2016 5 23

---
+++ src/main/java/com/basic/spark/Mlib/KMeans/OilSeaFieldAreaCluster.java
@@ -0,0 +1,66 @@
+package com.basic.spark.Mlib.KMeans;
+
+import com.basic.spark.util.DataInputUtils;
+import com.basic.spark.util.MlibUtils;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.Tuple2;
+
+import java.io.IOException;
+
+//第一个参数是聚类的类别   第二个参数是需要聚类的年份
+
+/**
+ * Created by xuzhanya on 16/5/21.
+ */
+public class OilSeaFieldAreaCluster {
+    private static final Logger log = LoggerFactory.getLogger(OilSeaFieldAreaCluster.class);
+
+    public static void main(String[] args) throws IOException {
+        SparkConf conf = new SparkConf().setAppName("OilSeaFieldAreaCluster").setMaster("local");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+
+        //数据导入
+        JavaPairRDD<ImmutableBytesWritable, Result> myRDD = DataInputUtils.getOilSeaFieldData(jsc,args);
+
+        if(args[0].equals("area")){
+            //如果参数是面积就按照面积进行聚类
+            final JavaRDD<Vector> parsedData=myRDD.map(new Function<Tuple2<ImmutableBytesWritable, Result>, Vector>() {
+                @Override
+                public Vector call(Tuple2<ImmutableBytesWritable, Result> resultTuple2) throws Exception {
+                    double[] values = new double[1];
+                    String Areavalue="";
+                    Result result=resultTuple2._2;
+                    Cell AreaCell=result.getColumnLatestCell("info".getBytes(),"MJ".getBytes());
+                    Areavalue=new String(CellUtil.cloneValue(AreaCell));
+
+                    //打印所有列族信息
+                    for(Cell cell : result.rawCells()){
+                        log.info("列簇为：" + new String(CellUtil.cloneFamily(cell)));
+                        log.info("列修饰符为：" + new String(CellUtil.cloneQualifier(cell)));
+                        log.info("值为：" + new String(CellUtil.cloneValue(cell)));
+                    }
+
+                    values[0]=Double.parseDouble(Areavalue);
+                    return Vectors.dense(values);
+                }
+            });
+
+            parsedData.cache();             //将数据缓存到内存中
+            int numClusters = 4;            //设置距离集群的个数
+            MlibUtils.KMeansModelOilField(numClusters,parsedData);
+        }
+
+    }
+}
