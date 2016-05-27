e69ad6aef1133325341be799327a22f08abd8370
谭杰 <798750509@qq.com>
2016-05-28 01:15:24 +0800

    完成了聚类结果写入关系型数据库的操作

--- src/main/java/com/basic/spark/Mlib/OilSeaField/OilSeaFieldStorageCluster.java
+++ src/main/java/com/basic/spark/Mlib/OilSeaField/OilSeaFieldStorageCluster.java
@@ -1,7 +1,9 @@
 package com.basic.spark.Mlib.OilSeaField;
 
 import com.basic.spark.DataInput;
-import com.basic.spark.util.MlibUtils;
+import com.basic.spark.dao.OilfieldStorageClusterDAO;
+import com.basic.spark.entity.OilfieldStorageCluster;
+import com.basic.spark.util.HBaseUtils;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.Result;
@@ -11,8 +13,12 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
@@ -25,8 +31,10 @@ import java.io.IOException;
  */
 public class OilSeaFieldStorageCluster {
     private static final Logger log = LoggerFactory.getLogger(OilSeaFieldStorageCluster.class);
-
+    private static String[] clusterType=new String[]{"石油储量油田1","石油储量油田2","石油储量油田3","石油储量油田4"};
     public static void main(String[] args) throws IOException {
+
+        final String NF=args[0];
         SparkConf conf = new SparkConf().setAppName("OilSeaFieldStorageCluster").setMaster("local");
         JavaSparkContext jsc = new JavaSparkContext(conf);
 
@@ -67,7 +75,62 @@ public class OilSeaFieldStorageCluster {
 
         parsedData.cache();             //将数据缓存到内存中
         int numClusters = 4;            //设置距离集群的个数
-        MlibUtils.KMeansModelOilField(numClusters,parsedData);
-    }
+        int numIterations = 20;         //设置算法迭代次数
+        final KMeansModel KmeansModel= KMeans.train(parsedData.rdd(), numClusters, numIterations);
+        final double coast=KmeansModel.computeCost(parsedData.rdd());
 
+        double[] centers=new double[4];
+        System.out.println("Cluster centers:");
+        for(int i=0;i<KmeansModel.clusterCenters().length;i++){
+            Vector center=KmeansModel.clusterCenters()[i];
+            centers[i]= center.apply(0);
+            System.out.println(" " + center);
+        }
+
+        //存储数据结果集到上层关系型数据库中
+        myRDD.foreach(new VoidFunction<Tuple2<ImmutableBytesWritable, Result>>() {
+            @Override
+            public void call(Tuple2<ImmutableBytesWritable, Result> resultTuple2) throws Exception {
+                Result r=resultTuple2._2;
+                String YQTBM= HBaseUtils.getColumnValueByResult(r,"info","YQTBM");
+                String YQTMC=HBaseUtils.getColumnValueByResult(r,"info","YQTMC");
+                String PDMC=HBaseUtils.getColumnValueByResult(r,"info","PDMC");
+                String YQCLX=HBaseUtils.getColumnValueByResult(r,"info","YQCLX");
+                Double CCB= Double.valueOf(HBaseUtils.getColumnValueByResult(r,"info","CCB"));
+                Double LJTMDZCLZL= Double.valueOf(HBaseUtils.getColumnValueByResult(r,"info","LJTMDZCLZL"));
+                Double LJTMDZCLTJ= Double.valueOf(HBaseUtils.getColumnValueByResult(r,"info","LJTMDZCLTJ"));
+
+                double[] values = new double[3];
+                values[0]=CCB;
+                values[1]=LJTMDZCLZL;
+                values[2]=LJTMDZCLTJ;
+                Vector vector=Vectors.dense(values);
+
+                int cluster_id=KmeansModel.predict(vector);
+                String cluster_type=clusterType[cluster_id];
+                log.info("YQTBM "+"油气田名称:"+YQTMC+" belong to "+cluster_id+" "+cluster_type);
+
+                //将数据存储到Mysql数据库中
+                OilfieldStorageClusterDAO storageClusterDAO=new OilfieldStorageClusterDAO();
+                Session session=storageClusterDAO.getSession();
+                session.beginTransaction();
+                OilfieldStorageCluster storageCluster=new OilfieldStorageCluster();
+                storageCluster.setClusterCost(coast);
+                storageCluster.setClusterId(cluster_id);
+                storageCluster.setPdmc(PDMC);
+                storageCluster.setCcb(CCB);
+                storageCluster.setLjtmdzcltj(LJTMDZCLTJ);
+                storageCluster.setLjtmdzclzl(LJTMDZCLZL);
+                storageCluster.setNf(NF);
+                storageCluster.setYqclx(YQCLX);
+                storageCluster.setYqtbm(YQTBM);
+                storageCluster.setYqtmc(YQTMC);
+                storageCluster.setClusterType(cluster_type);
+                session.save(storageCluster);
+                session.getTransaction().commit();
+                session.close();
+            }
+        });
+        jsc.stop();
+    }
 }
--- src/main/java/com/basic/spark/Mlib/OilSeaField/OilSeaFieldTMCDCluster.java
+++ src/main/java/com/basic/spark/Mlib/OilSeaField/OilSeaFieldTMCDCluster.java
@@ -1,7 +1,10 @@
 package com.basic.spark.Mlib.OilSeaField;
 
 import com.basic.spark.DataInput;
-import com.basic.spark.util.MlibUtils;
+import com.basic.spark.dao.OilfieldTmcdClusterDAO;
+import com.basic.spark.entity.OilfieldTmcdCluster;
+import com.basic.spark.util.ClusterUtils;
+import com.basic.spark.util.HBaseUtils;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.Result;
@@ -11,8 +14,12 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Tuple2;
@@ -29,7 +36,9 @@ import java.io.IOException;
  */
 public class OilSeaFieldTMCDCluster {
     private static final Logger log = LoggerFactory.getLogger(OilSeaFieldTMCDCluster.class);
+    private static String[] clusterType=new String[]{"小型探明程度油田","中等探明程度油田","偏大探明程度油田","大型探明程度油田"};
     public static void main(String[] args) throws IOException {
+        final String NF=args[0];
         SparkConf conf = new SparkConf().setAppName("OilSeaFieldTMCDCluster").setMaster("local");
         JavaSparkContext jsc = new JavaSparkContext(conf);
 
@@ -59,7 +68,53 @@ public class OilSeaFieldTMCDCluster {
 
         parsedData.cache();             //将数据缓存到内存中
         int numClusters = 4;            //设置距离集群的个数
-        MlibUtils.KMeansModelOilField(numClusters,parsedData);
+        int numIterations = 20;         //设置算法迭代次数
+        final KMeansModel KmeansModel= KMeans.train(parsedData.rdd(), numClusters, numIterations);
+        final double coast=KmeansModel.computeCost(parsedData.rdd());
 
+        double[] centers=new double[4];
+        System.out.println("Cluster centers:");
+        for(int i=0;i<KmeansModel.clusterCenters().length;i++){
+            Vector center=KmeansModel.clusterCenters()[i];
+            centers[i]= center.apply(0);
+            System.out.println(" " + center);
+        }
+
+        //存储数据结果集到上层关系型数据库中
+        myRDD.foreach(new VoidFunction<Tuple2<ImmutableBytesWritable, Result>>() {
+            @Override
+            public void call(Tuple2<ImmutableBytesWritable, Result> resultTuple2) throws Exception {
+                Result r=resultTuple2._2;
+                String YQTBM= HBaseUtils.getColumnValueByResult(r,"info","YQTBM");
+                String YQTMC=HBaseUtils.getColumnValueByResult(r,"info","YQTMC");
+                String PDMC=HBaseUtils.getColumnValueByResult(r,"info","PDMC");
+                String YQCLX=HBaseUtils.getColumnValueByResult(r,"info","YQCLX");
+                double TMCD= Double.valueOf(HBaseUtils.getColumnValueByResult(r,"info","TMCD"));
+                Vector vector=Vectors.dense(new double[]{TMCD});
+
+                int cluster_id=KmeansModel.predict(vector);
+                String cluster_type= ClusterUtils.getClustrType(clusterType,KmeansModel.clusterCenters(),vector);
+                log.info("YQTBM "+"油气田名称:"+YQTMC+" 油气田探明程度: "+TMCD+" belong to "+cluster_id+" "+cluster_type);
+
+                //将数据存储到Mysql数据库中
+                OilfieldTmcdClusterDAO tmcdClusterDAO =new OilfieldTmcdClusterDAO();
+                Session session=tmcdClusterDAO.getSession();
+                session.beginTransaction();
+                OilfieldTmcdCluster tmcdCluster=new OilfieldTmcdCluster();
+                tmcdCluster.setClusterCost(coast);
+                tmcdCluster.setClusterId(cluster_id);
+                tmcdCluster.setPdmc(PDMC);
+                tmcdCluster.setTmcd(TMCD);
+                tmcdCluster.setNf(NF);
+                tmcdCluster.setYqclx(YQCLX);
+                tmcdCluster.setYqtbm(YQTBM);
+                tmcdCluster.setYqtmc(YQTMC);
+                tmcdCluster.setClusterType(cluster_type);
+                session.save(tmcdCluster);
+                session.getTransaction().commit();
+                session.close();
+            }
+        });
+        jsc.stop();
     }
 }
